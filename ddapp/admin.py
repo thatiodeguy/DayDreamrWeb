6ddb24cbaa5f60653109dfdd890d7f4852587e0d
Adam <107372600+thatiodeguy@users.noreply.github.com>
2023-10-03 09:24:09 -0700

    Update admin.py
    registered passwordresettoken model

--- ddapp/admin.py
+++ ddapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Roadmap, Task, CustomUser
+from .models import Roadmap, Task, CustomUser, PasswordResetToken
 
 # Register your models here.
 admin.site.register(Roadmap)
 admin.site.register(Task)
 admin.site.register(CustomUser)
+admin.site.register(PasswordResetToken)
